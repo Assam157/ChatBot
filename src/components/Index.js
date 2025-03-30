b4ea593522bae64f05d4ba1c6c82bda1ded68b0e
Assam157 <asasm8106@gmail.com>
2025-03-31 00:47:46 +0530

    Update Index.js

--- src/components/Index.js
+++ src/components/Index.js
@@ -12,7 +12,7 @@ const App = () => {
     setMessages([...messages, userMessage]);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/chat", {
+      const response = await fetch("https://chatbotservicebackend.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
